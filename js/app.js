fdc83840803faac0c7c788b522ece73cd2fdf35e
Jeremy Yochum <jyochum391@yahoo.com>
2023-04-16 18:16:42 -0400

    update phrases array

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const startButton = document.querySelector('.btn__reset');
 const phrases = [
     'I LIKE TURTLES',
     'HELLO WORLD',
-    'ALLS WELL THAT ENDS WELL',
+    'WHERE THERE IS A WILL THERE IS A WAY',
     'WHEN IN ROME',
     'A BIRD IN THE HAND IS WORTH TWO IN THE BUSH'
 ];
