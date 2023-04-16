e5973c86d51a509d9ae76af9aaded6b1c01a78e2
Jeremy Yochum <jyochum391@yahoo.com>
2023-04-16 18:12:55 -0400

    add phrases array

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ const board = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startScreen = document.querySelector('#overlay');
 const startButton = document.querySelector('.btn__reset');
+const phrases = [
+    'I LIKE TURTLES',
+    'HELLO WORLD',
+    'ALLS WELL THAT ENDS WELL',
+    'WHEN IN ROME',
+    'A BIRD IN THE HAND IS WORTH TWO IN THE BUSH'
+];
 let missed = 0;
 
 
